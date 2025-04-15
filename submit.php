970b50ab3b37ce758af0a5b35ff9e4751ce34281
affwhitepages <affwhitepages@gmail.com>
2025-04-15 14:30:49 -0400

    Add files via upload

---
+++ submit.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Osobisty coach Aleksander Biały z Polski oferuje profesjonalne usługi w zakresie rozwoju osobistego, osiągania celów i poprawy jakości życia. Zainwestuj w siebie z naszym doświadczeniem!">
+        <meta name="keywords" content="osobisty coach, Aleksander Biały, rozwój osobisty, osiąganie celów, coaching, Polska, poprawa jakości życia">
+        <title>Osobisty Coach Aleksander Biały - Dziękujemy</title>
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap-icons.css">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+      </head>
+<body>
+    <div class="container my-5 text-center">
+        <div class="alert" role="alert">
+            <i class="bi bi-check-circle-fill" style="font-size: 50px;"></i>
+            <h1 class="mt-3">Dziękujemy!</h1>
+            <p>Twoje zgłoszenie zostało pomyślnie wysłane. Skontaktujemy się z Tobą wkrótce.</p>
+            <p>Dziękujemy za zaufanie.</p>
+            <a href="/" class="btn btn-danger mt-3">Powrót do Strony Głównej</a>
+        </div>
+    </div>
+</body>
+</html>
